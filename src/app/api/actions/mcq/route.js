bce96e63f75571f7b8635142ed80f1ec70ed2d38
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-04-09 11:03:51 +0530

    added gemini mcq ai

--- src/app/api/actions/mcq/route.js
+++ src/app/api/actions/mcq/route.js
@@ -2,9 +2,12 @@ export const runtime = "nodejs";
 
 import { getServerSession } from "next-auth";
 import { extractAndStoreText } from "../../../../lib/pdfService";
-import { getText } from "../../../../lib/getText";
+import { getText } from "../../../../lib/text";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 //import { prisma } from "../../../../lib/prisma";
 export async function POST(req) {
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+  console.log("KEY:", process.env.GEMINI_API_KEY);
   try {
     const session = await getServerSession();
     if (!session || !session.user) {
@@ -17,8 +20,6 @@ export async function POST(req) {
       return Response.json({ error: "No file uploaded" }, { status: 400 });
     }
     const buffer = Buffer.from(await file.arrayBuffer());
-    const { textId } = await extractAndStoreText(buffer, userId);
-    const { text } = await getText({ textId });
     // const textFounded = await prisma.text.findUnique({
     //   where: { id: textId },
     // });
@@ -32,6 +33,52 @@ export async function POST(req) {
     //   textId,
     //   preview: text.slice(0, 300),
     // });
+    const { textId } = await extractAndStoreText(buffer, userId);
+    const { text } = await getText({ textId });
+
+    if (text) {
+      const prompt = `
+You are an expert teacher.
+
+Generate high-quality multiple choice questions (MCQs) from the given ${text}.
+
+Instructions:
+- Create exactly 5 questions
+- Each question must test understanding, not just memorization
+- Avoid very easy or trivial questions
+- Focus on important concepts only
+
+Format:
+Return ONLY valid JSON. Do not include any explanation or extra text.
+
+JSON structure:
+{
+  "questions": [
+    {
+      "question": "string",
+      "options": ["A", "B", "C", "D"],
+      "correctAnswer": "exact option text"
+    }
+  ]
+}
+
+Rules:
+- Each question must have exactly 4 options
+- Only ONE correct answer per question
+- Do not repeat similar questions
+- Keep options clear and distinct
+- Do not include numbering like "Q1", "Q2"
+- Do not include markdown or backticks
+
+Now generate questions from the following text:
+`;
+      const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+      const result = await model.generateContent(prompt);
+      const response = result.response.text();
+      console.log("RESULT:", response);
+      return Response.json({ response: response });
+    }
+    //prefer gemini-2.5-flash for best performance
   } catch (e) {
     console.error(e);
     return Response.json(
