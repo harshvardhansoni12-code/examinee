e3587ba4e8b3d6fadef7ee49eec9cd0440eef74c
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-04-14 15:56:29 +0530

    fixed card and summary routes

---
+++ src/app/api/actions/get-cards/route.js
@@ -0,0 +1,20 @@
+import { getServerSession } from "next-auth";
+import { prisma } from "../../../../lib/prisma";
+export async function GET(req) {
+  const session = await getServerSession();
+
+  const mcqFound = await prisma.cards.findFirst({
+    where: {
+      author: {
+        email: session.user.email,
+      },
+    },
+    orderBy: {
+      id: "desc", // latest
+    },
+  });
+  if (!mcqFound) {
+    return Response.json("mcq not found", { status: 400 });
+  }
+  return Response.json([mcqFound], { status: 200 });
+}
---
+++ src/app/api/actions/get-summary/route.js
@@ -0,0 +1,28 @@
+import { getServerSession } from "next-auth";
+import { prisma } from "../../../../lib/prisma";
+export async function GET(req) {
+  const session = await getServerSession();
+
+  const summaryFound = await prisma.summary.findFirst({
+    // where: {
+    //   author: {
+    //     email: session.user.email,
+    //   },
+    // },
+    // orderBy: {
+    //   id: "desc", // latest
+    // },
+    where: {
+      author: {
+        email: session.user.email,
+      },
+    },
+    orderBy: {
+      id: "desc",
+    },
+  });
+  if (!summaryFound) {
+    return Response.json("mcq not found", { status: 400 });
+  }
+  return Response.json([summaryFound], { status: 200 });
+}
